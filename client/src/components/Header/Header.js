e322e6c2e4ce6df1193aee37f75862c9091acd59
Erik Hoversten <ehoversten@gmail.com>
2023-04-02 17:14:39 -0400

    completed baseline content and testing for Header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import './Header.css';
 
 function Header() {
+    const [location, setLocation] = useState('Location, Title');
+
     return (
         <header className="header">
             <img src="" alt="high tides logo" />
             <h1>High Tides</h1>
             <div className="location">
-                <p>for <span id="location-title">Location, Title</span></p>
+                <p>for <span id="location-title">{location}</span></p>
             </div>
         </header>
     );
--- client/src/components/Header/Header.test.js
+++ client/src/components/Header/Header.test.js
@@ -1,20 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
-test('renders application title', () => {
-    render(<Header />);
-    const titleElement = screen.getByText('High Tides');
-    expect(titleElement).toBeInTheDocument();
-});
+describe("Tests for Header Component", () => {
 
-test('renders application logo', () => {
-    render(<Header />);
-    const logoElement = screen.getByAltText('high tides logo');
-    expect(logoElement).toBeInTheDocument();
-});
-
-test('renders text for location of API request', () => {
-    render(<Header />);
-    const locationElement = screen.getByText('for');
-    expect(locationElement).toBeInTheDocument();
+    test('renders application title', () => {
+        render(<Header />);
+        const titleElement = screen.getByText('High Tides');
+        expect(titleElement).toBeInTheDocument();
+    });
+    
+    test('renders application logo', () => {
+        render(<Header />);
+        const logoElement = screen.getByAltText('high tides logo');
+        expect(logoElement).toBeInTheDocument();
+    });
+    
+    test('renders text for location of API request', () => {
+        render(<Header />);
+        const locationElement = screen.getByText('for');
+        expect(locationElement).toBeInTheDocument();
+    });
+    
+    test('renders a heading element in the DOM/JSX', () => {
+        render(<Header />);
+        const headinglement = screen.getByRole('heading');
+        expect(headinglement).toBeInTheDocument();
+    });
+    
+    test('renders a heading element with text of "High Tides" in the DOM/JSX', () => {
+        render(<Header />);
+        const headinglement = screen.getByRole('heading', { name: "High Tides"});
+        expect(headinglement).toBeInTheDocument();
+    });
+    
 })

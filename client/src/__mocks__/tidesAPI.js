a4cb88190d3963a12b6ac2bbdf0b61f05c197106
Erik Hoversten <ehoversten@gmail.com>
2023-04-11 17:30:12 -0400

    cleaning up mock files

--- client/src/__mocks__/tidesAPI.js
+++ client/src/__mocks__/tidesAPI.js
@@ -1,5 +1,6 @@
-export default { 
-    searchTides: Promise.resolve({ 
+
+const searchTides = () => {
+    return Promise.resolve({
         data: { 
             predictions: [
                 { t: "1900-01-01 06:00", v: "1.5", type: 'H' },
@@ -11,17 +12,4 @@ export default {
     })
 }
 
-// const searchTides = () => {
-//     return Promise.resolve({
-//         data: { 
-//             predictions: [
-//                 { t: "1900-01-01 06:00", v: "1.5", type: 'H' },
-//                 { t: "1900-01-01 10:00", v: "0.5", type: 'L' },
-//                 { t: "1900-01-01 14:00", v: "1.5", type: 'H' },
-//                 { t: "1900-01-01 18:00", v: "0.5", type: 'L' },
-//             ]
-//         }
-//     })
-// }
-
-// export default searchTides;
+export default searchTides;
